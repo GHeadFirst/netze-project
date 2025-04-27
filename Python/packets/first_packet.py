4119bb691a318411debd7258f83bdce94a61ecc2
fares <alfaresbakri@gmail.com>
2025-04-28 01:10:14 +0200

    made packet an abstract class, and added created socket on receiver

--- Python/packets/first_packet.py
+++ Python/packets/first_packet.py
@@ -1,4 +1,16 @@
 class FirstPacket:
-    def __init__(self, transmission_id, sequence_number):
-        self.transmission_id = transmission-id
-        self.sequence_number = sequence-number
+    def __init__(self, transmission_id: int, sequence_number: int ) -> None: 
+        self.transmission_id = transmission_id
+        self.sequence_number = sequence_number
+
+    def get_transmission_id(self) -> int:
+        return self.transmission_id
+    
+    def get_sequence_number(self) -> int:
+        return self.sequence_number
+
+    def get_max_sequence_number(self) -> int:
+            return self.get_max_sequence_number
+
+    def get_file_name(self) -> str:
+            return self.get_file_name
