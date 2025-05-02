dd4f2013c7a712db9e6304ce22d6027f5d27a5af
fares <alfaresbakri@gmail.com>
2025-05-02 18:34:59 +0200

    changed from abstract methods to normal methods

--- Python/packets/data_packet.py
+++ Python/packets/data_packet.py
@@ -5,11 +5,6 @@ class DataPacket(Packet):
         super.__init__(transmission_id, sequence_number)
         self.data = data
 
-    def get_transmission_id(self) -> int:
-        return self.transmission_id
-    
-    def get_sequence_number(self) -> int:
-        return self.sequence_number
 
     def get_data(self) -> bytes:
         return self.data
--- Python/packets/last_packet.py
+++ Python/packets/last_packet.py
@@ -5,11 +5,6 @@ class LastPacket(Packet):
         super().__init__(transmission_id, sequence_number)
         self.md5 = md5
 
-    def get_transmission_id(self) -> int:
-        return self.transmission_id
-    
-    def get_sequence_number(self) -> int:
-        return self.sequence_number
     
     def get_md5(self) -> str:
         return self.md5
