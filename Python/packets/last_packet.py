80c9b1c79b7562ac5dcab4705e5c307af459f36a
KaisKhalifa11 <Kais.Khalifa@stud.plus.ac.at>
2025-05-02 20:07:23 +0200

    20 Uhr

--- Python/packets/last_packet.py
+++ Python/packets/last_packet.py
@@ -1,10 +1,17 @@
+import struct
 from packet import Packet
 
 class LastPacket(Packet):
-    def __init__(self, transmission_id: int, sequence_number: int, md5: str) -> None:
+    def __init__(self, transmission_id: int, sequence_number: int, md5: bytes) -> None:
         super().__init__(transmission_id, sequence_number)
-        self.md5 = md5
+        if len(self.md5 != 16):
+            raise ValueError("falsche Größe von MD5!")
+        self.md5 = md5(16)
 
+    def serialisieren(self) -> bytes:
+        header = struct.pack("!HI", self.transmission_id, self.sequence_number)
+        return header + self.md5
+     
     
-    def get_md5(self) -> str:
+    def get_md5(self) -> bytes:
         return self.md5
