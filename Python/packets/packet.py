1952ebd28014d5c338f0de65c5917081d6f4a4a4
Adnan Memic <adnan.adem.am@gmail.com>
2025-05-02 17:10:01 +0200

    add constructor to packet

--- Python/packets/packet.py
+++ Python/packets/packet.py
@@ -1,6 +1,9 @@
 from abc import ABC, abstractmethod
 
 class Packet(ABC):
+    def __init__(self, transmission_id: int, sequence_number: int ) -> None: 
+        self.transmission_id = transmission_id
+        self.sequence_number = sequence_number
 
     @abstractmethod
     def get_transmission_id(self) -> int:
