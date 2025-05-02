81217f7a383855c6c12c94f4ac130ae4e43db64e
KaisKhalifa11 <Kais.Khalifa@stud.plus.ac.at>
2025-05-02 15:57:20 +0200

    last_packet

---
+++ Python/packets/last_packet.py
@@ -0,0 +1,14 @@
+class LastPacket:
+    def __init__(self, transmission_id: int, sequence_number: int, md5: str) -> None:
+        self.transmission_id = transmission_id
+        self.sequence_number = sequence_number
+        self.md5 = md5
+
+    def get_transmission_id(self) -> int:
+        return self.transmission_id
+    
+    def get_sequence_number(self) -> int:
+        return self.sequence_number
+    
+    def get_md5(self) -> str:
+        return self.md5
