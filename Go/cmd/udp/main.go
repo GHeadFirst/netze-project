6a00e6c34ebd24191c9c67cf27d801cc5667fe3f
Adnan Memic <adnan.adem.am@gmail.com>
2025-05-04 00:18:40 +0200

    add function calls to main

--- Go/cmd/udp/main.go
+++ Go/cmd/udp/main.go
@@ -1,10 +1,16 @@
 package main
 
 import (
+	"time"
+
 	"github.com/GHeadFirst/netze-project/Go/internal/net"
 )
 
 func main() {
+	// go routine to run the receiver simultanously with the transmitter
+	go net.Receive()
+	// enough time so the receiver can start
+	time.Sleep(100 * time.Millisecond)
 	// send data
-	net.Transmission("test_data_go.txt")
+	net.Transmission("./test_data_go.txt")
 }
