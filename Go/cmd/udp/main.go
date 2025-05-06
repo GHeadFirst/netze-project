bf3deb480296b7f74238e1d0ff1faa8ba95279c2
KaisKhalifa11 <kais.khalifa@stud.plus.ac.at>
2025-05-06 17:29:32 +0200

    go flags, but not done!

--- Go/cmd/udp/main.go
+++ Go/cmd/udp/main.go
@@ -1,14 +1,28 @@
 package main
 
 import (
+	"flag"
 	"time"
 
 	"github.com/GHeadFirst/netze-project/Go/internal/net"
 )
 
 func main() {
+	var (
+		send    string
+		receive string
+	)
+	flag.StringVar(&send, "send", "", "Sender file")
+	flag.StringVar(&receive, "receive", "", "Receiver file")
 
-	net.Receiver()
+	if send == "send" {
+		net.Transmitter("picture.jpg")
+
+	}
+
+	if receive == "receive" {
+		net.Receiver()
+	}
 
 	time.Sleep(10 * time.Millisecond)
 	//net.Transmitter("picture.jpg")
